f78dde235b7e789b69a04f5c611a2808140377d7
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-03-13 01:18:59 -0300

    dados mockados

--- server.js
+++ server.js
@@ -2,4 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+let medicos = [
+  { id: 1, nome: "Dr. João Silva", especialidade: "Cardiologia" },
+  { id: 2, nome: "Dra. Maria Oliveira", especialidade: "Pediatria" },
+  { id: 3, nome: "Dr. Carlos Souza", especialidade: "Ortopedia" },
+];
+
+let dentistas = [
+  { id: 1, nome: "Dr. Ana Costa", especialidade: "Ortodontia" },
+  { id: 2, nome: "Dr. Pedro Lima", especialidade: "Endodontia" },
+  { id: 3, nome: "Dra. Luisa Fernandes", especialidade: "Periodontia" },
+];
+
+let medicosDentistas = [...medicos, ...dentistas];
+
 app.use(express.json());
